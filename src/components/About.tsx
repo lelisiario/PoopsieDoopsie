b8077bfc9c479a479ecbf33b38f08fa8cc73f372
Lauren Elisiario <lauren.elisiario@gmail.com>
2025-04-05 10:21:44 -0700

    comnmentedo ut line of code

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,12 +33,14 @@ const About: React.FC = () => {
                   </svg>
                   <span>Reliable & Consistent</span>
                 </div>
-                <div className="flex items-center">
-                  <svg className="check-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-                  </svg>
-                  <span>Fully Insured</span>
-                </div>
+    {/* 
+    <div className="flex items-center">
+      <svg className="check-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+      </svg>
+      <span>Fully Insured</span>
+    </div>
+    */}
                 <div className="flex items-center">
                   <svg className="check-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
