87d3406ec921965817dd52fb4d818db3fde91771
Lauren Elisiario <lauren.elisiario@gmail.com>
2025-04-05 23:30:00 -0700

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,24 @@
-
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container navbar-container">
         <div className="logo">
-          <a href="/">
+          <Link to="/">
             <span className="logo-text">Poopsie Doopsie</span>
             <span className="logo-tag">Pet Waste Removal</span>
-          </a>
+          </Link>
         </div>
         <div className="nav-links">
           <a href="#services">Services</a>
           <a href="#pricing">Pricing</a>
           <a href="#about">About Us</a>
-          <a href="#contact">Contact</a>
+          <Link to="/contact">Contact</Link>
         </div>
         <div className="cta-button">
-          <a href="#contact" className="btn btn-primary">Get Started</a>
+          <Link to="/contact" className="btn btn-primary">Get Started</Link>
         </div>
       </div>
     </nav>
