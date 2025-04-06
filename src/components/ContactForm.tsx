6bd5db6714866b8165e7d0a7e3b82a9c43ddca06
Lauren Elisiario <lauren.elisiario@gmail.com>
2025-04-05 22:56:56 -0700

    Removed commented code. I don't need it.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -161,37 +161,6 @@ const ContactForm: React.FC = () => {
             )}
           </div>
         </div>
-{/*         
-        <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
-          <div>
-            <div className="bg-green-500 h-12 w-12 rounded-full flex items-center justify-center mx-auto mb-4">
-              <svg className="h-6 w-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-              </svg>
-            </div>
-            <h3 className="text-xl font-semibold mb-2">Call Us</h3>
-            <p>(555) 123-4567</p>
-          </div>
-          <div>
-            <div className="bg-green-500 h-12 w-12 rounded-full flex items-center justify-center mx-auto mb-4">
-              <svg className="h-6 w-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-              </svg>
-            </div>
-            <h3 className="text-xl font-semibold mb-2">Email Us</h3>
-            <p>info@poopsiedoopsie.com</p>
-          </div>
-          <div>
-            <div className="bg-green-500 h-12 w-12 rounded-full flex items-center justify-center mx-auto mb-4">
-              <svg className="h-6 w-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-              </svg>
-            </div>
-            <h3 className="text-xl font-semibold mb-2">Service Area</h3>
-            <p>Vacaville, CA and surrounding areas</p>
-          </div>
-        </div> */}
       </div>
     </section>
   );
